1472f8b414904010f4c5d2ab10c29abf8f9776e3
Javier Castillo <javiercame98@gmail.com>
2026-02-11 10:18:30 +0100

    initial commit

--- src/main/java/org/example/CarService.java
+++ src/main/java/org/example/CarService.java
@@ -3,12 +3,6 @@ package org.example;
 import java.util.ArrayList;
 import java.util.List;
 
-//1. **Listar todos los coches registrados**, para mostrar su información relevante.
-//2. **Filtrar los coches fabricados después de un año específico**, proporcionado como parámetro.
-//3. **Filtrar los coches por tipo y combustible**, devolviendo únicamente aquellos que coincidan con:
-//    - Un **tipo de coche** concreto (Sedán, SUV o Deportivo).
-//    - Un **tipo de combustible** concreto (gasolina, diésel o eléctrico).
-
 public class CarService {
 
     private List<Car> cars = new ArrayList<>();
@@ -36,7 +30,7 @@ public class CarService {
 
 
 
-    public List<Car> filterByTypeAndFuel(Car car, TypeFuel fuel){
+    public List<Car> filterByTypeAndFuel(Car car, TypeFuel fuel) {
         return cars.stream()
                 .filter(cars -> cars instanceof Car type && type.getTypeFuel() == fuel)
                 .toList();
